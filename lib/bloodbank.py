9a980b7197e7ace112f808115fe3161e2a675086
pranjay01 <pranjay.sagar01@gmail.com>
2020-04-18 15:19:57 -0700

    chabges

--- lib/bloodbank.py
+++ lib/bloodbank.py
@@ -1,5 +1,6 @@
 import mysql.connector as mysql
 from connection import get_connection
+from user import Operator
 
 class Bloodbank:
     def __init__(self,Bbank_id,Name,Type,Phone_no):
@@ -27,9 +28,9 @@ class Bloodbank:
         db=get_connection()
         cursor = db.cursor()
 
-        delete_query=f"DELETE FROM BLOOD_BANK WHERE Bbank_id = '{bloodbank['Bbank_id']}'"
+        delete_query="DELETE FROM BLOOD_BANK WHERE Bbank_id =%s"
         try:
-            cursor.execute(delete_query)
+            cursor.execute(delete_query,(bloodbank['Bbank_id'],))
             db.commit()
         except mysql.Error as err:
             print("Failed to delete entry: {}".format(err))
@@ -58,9 +59,9 @@ class Bloodbank:
                 db.close()
         #when information of only particular blood bank is required    
         elif bloodbank["case"] == 2:
-            get_query=f"SELECT * FROM BLOOD_BANK WHERE Bbank_id = '{bloodbank['Bbank_id']}'"
+            get_query="SELECT * FROM BLOOD_BANK WHERE Bbank_id = %s"
             try:
-                cursor.execute(get_query)
+                cursor.execute(get_query,(bloodbank['Bbank_id'],))
                 row = cursor.fetchone()
                 return {"status": 200, "result":self(*row)}
             except mysql.Error as err:
@@ -136,7 +137,67 @@ class Bloodbank:
         # #     finally:
         # #         db.close()
 
-        
+class BloodBankBranch:
+    def __init__(self,Br_id,Br_Type,Bbank_id,Street,City,Zip):
+        self.Br_id =Br_id
+        self.Br_Type =Br_Type
+        self.Bbank_id = Bbank_id
+        self.Street =Street
+        self.City = City
+        self.Zip =Zip
+
+    @classmethod
+    def creat_new_branch(self,branch,Operator_id):
+        if Operator.check_bankid(Operator_id,branch["Bbank_id"]):       
+            db=get_connection()
+            cursor = db.cursor()
+            try:
+                insert_query="INSERT INTO BRANCH (Br_Type,Bbank_id,Street,City,Zip)  VALUES (%s,%s,%s,%s,%s)"
+                cursor.execute(insert_query,( branch['Br_Type'],branch['Bbank_id'],branch['Street'],branch['City'],branch['Zip']))
+                db.commit()
+                return {"status":201, "message":"New branch created", "branch": branch}
+            except mysql.Error as err:
+                return {"status": 500, "message": str(err)} 
+            finally:
+                db.close()
+        else:
+            return {"status": 401, "message": "Unauthorised Access"}
+
+    @classmethod
+    def update_branch(self,branch,Operator_id):
+        if Operator.check_branch_id(Operator_id,branch["Br_id"]):       
+            db=get_connection()
+            cursor = db.cursor()
+            try:
+                update_query="UPDATE BRANCH set Br_Type=%s, Street=%s, City=%s, Zip=%s where Br_id=%s"
+                cursor.execute(update_query,( branch['Br_Type'],branch['Street'],branch['City'],branch['Zip'],branch['Br_id']))
+                db.commit()
+                return {"status":200, "message":"Branch updated"}
+            except mysql.Error as err:
+                return {"status": 500, "message": str(err)} 
+            finally:
+                db.close()
+        else:
+            return {"status": 401, "message": "Unauthorised Access"}
+ 
+         
+    @classmethod
+    def delete_delete(self,branch,Operator_id):
+        if Operator.check_branch_id(Operator_id,branch["Br_id"]):
+            db=get_connection()
+            cursor = db.cursor()
+            try:
+                delete_query="DELETE FROM BRANCH where Br_id=%s"
+                cursor.execute(delete_query,(branch['Br_id']))
+                db.commit()
+                return {"status":200, "message":"Branch deleted successfully"}
+            except mysql.Error as err:
+                return {"status": 500, "message": str(err)} 
+            finally:
+                db.close()
+        else:
+            return {"status": 401, "message": "Unauthorised Access"}
+       
 
         
 
