45df11dd917028282ab640daa003780218e361f6
Salman Mal <salmanmal@yahoo.com>
2020-05-08 01:25:16 -0700

    removed front-end from here

--- front-end/src/components/Loader/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './index.scss'
-export default function index() {
-    return (
-        <div className="loading">Loading&#8230;</div>
-    )
-}
--- front-end/src/containers/snackBar/Message.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function Message(props) {
-  const { data } = props;
-  return <div className={data.success ? "success" : "fail"}>{data.error}</div>;
-}
